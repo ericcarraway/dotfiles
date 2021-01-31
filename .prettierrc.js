2cea7f1c5345b250ca17d7e0f13b6839d11ce27e
Eric Carraway <eric@ericcarraway.com>
2021-01-31 16:16:32 +0100

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,27 @@
 module.exports = {
-  arrowParens: "always",
+  arrowParens: `always`,
   bracketSpacing: true,
-  embeddedLanguageFormatting: "auto",
-  htmlWhitespaceSensitivity: "css",
+  embeddedLanguageFormatting: `auto`,
+  htmlWhitespaceSensitivity: `css`,
   insertPragma: false,
   jsxBracketSameLine: false,
   jsxSingleQuote: false,
   printWidth: 80,
-  proseWrap: "preserve",
-  quoteProps: "as-needed",
+  proseWrap: `preserve`,
+  quoteProps: `as-needed`,
   requirePragma: false,
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: `all`,
   useTabs: false,
   vueIndentScriptAndStyle: false,
+  overrides: [
+    {
+      files: [`*.yml`],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
